904477ea13ccf8d706874b4e2c1defaa73d93cc6
sdim-ua <ingersol@yandex.ua>
2018-12-19 00:08:31 +0400

    Create Solution.java

---
+++ Solution.java
@@ -0,0 +1,30 @@
+/* 
+Питомцам нужны люди
+*/
+public class Solution {
+    public static void main(String[] args) {
+        //напишите тут ваш код
+        Woman woman = new Woman();
+        Cat cat = new Cat();
+        Dog dog = new Dog();
+        Fish fish = new Fish();
+        cat.owner = woman;
+        dog.owner = woman;
+        fish.owner = woman;
+    }
+
+    public static class Cat {
+        public Woman owner;
+    }
+
+    public static class Dog {
+        public Woman owner;
+    }
+
+    public static class Fish {
+        public Woman owner;
+    }
+
+    public static class Woman {
+    }
+}
